4ec2c9dcc2dce21ac4c6ad5668b448a8de22ff98
Rakshith <rakshithx09@gmail.com>
2024-03-14 18:51:33 +0530

    Mui setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="w-[80%] h-[3rem] mt-[3rem] mx-auto border-black border-2"></nav>
+        <nav className="h-[60%] w-[3rem]   border-black border-2 fixed left-[4rem] top-1/2 transform -translate-y-1/2 "></nav>
+        {children}</body>
     </html>
   );
 }
