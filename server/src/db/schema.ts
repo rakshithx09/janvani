c71f7260f686b34951d2405722e1fee6d7c2cca4
Karthik-S-Salian <karthikssalian5@gmail.com>
2024-03-14 21:46:57 +0530

    updated schemas

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,31 +1,41 @@
 import { sql } from 'drizzle-orm'
-import { integer, sqliteTable, text, uniqueIndex } from 'drizzle-orm/sqlite-core'
-export const todos = sqliteTable(
-    'todos',
-    {
-        id: integer('id').primaryKey(),
-        title: text('title').notNull(),
-        description: text('description').notNull(),
-        dueDate: integer('due_date', {
-            mode: 'timestamp',
-        }).notNull(),
-        priority: text('priority', { enum: ['High', 'Medium', 'Low'] }).notNull(),
-        status: text('status', {
-            enum: ['To-Do', 'In Progress', 'Completed'],
-        })
-            .notNull()
-            .default('To-Do'),
-        createdAt: integer('created_at')
-            .default(sql`CURRENT_TIMESTAMP`)
-            .notNull(),
-        updatedAt: integer('updated_at')
-            .default(sql`CURRENT_TIMESTAMP`)
-            .notNull(),
-        completedAt: integer('completed_at', {
-            mode: 'timestamp',
-        }),
-    },
-    (todos) => ({
-        nameIdx: uniqueIndex('nameIdx').on(todos.title),
-    })
-)
+import { integer, sqliteTable, text, uniqueIndex, primaryKey } from 'drizzle-orm/sqlite-core'
+
+export const userTable = sqliteTable('user', {
+    id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+    name: text("name").notNull(),
+    email: text("email").notNull().unique(),
+    password: text("password").notNull(),
+    joinedOn: integer('joined_on')
+        .default(sql`CURRENT_TIMESTAMP`)
+        .notNull()
+});
+
+export const postTable = sqliteTable('post', {
+    id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+    title: text("title").notNull(),
+    desciption: text("desciption").notNull(),
+    image: text("image"),
+    complaintType: text('complaintType', { enum: ['association', 'group', 'individual', 'individual'] }).notNull(),
+    department: text('department', { enum: ['engineering', 'public health', 'revenue', 'town planning'] }).notNull(),
+    userId: integer("author_id").references(() => userTable.id).notNull(),
+    createdAt: integer('created_at')
+        .default(sql`CURRENT_TIMESTAMP`)
+        .notNull()
+});
+
+// export const tagsTable = sqliteTable('Tag', {
+//     id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+//     tag: text("tag").notNull().unique(),
+//     recipeId: integer("recipe_id").references(() => recipeTable.id).notNull(),
+// });
+
+export const commemtsTable = sqliteTable('Comment', {
+    id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+    userId: integer("user_id").references(() => userTable.id).notNull(),
+    postId: integer("postId").references(() => postTable.id).notNull(),
+    content: text("content"),
+    createdAt: integer('created_at')
+        .default(sql`CURRENT_TIMESTAMP`)
+        .notNull()
+});
