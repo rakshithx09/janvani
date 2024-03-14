7506980db2ecb525906bb81fe1ee624ac2a561bc
VarshithPawarHR <varshithpawarhr@gmail.com>
2024-03-15 01:49:44 +0530

    conflict resolved

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Box } from "@mui/material";
-import { Avatar } from '@mui/material';
+import { Avatar } from "@mui/material";
 import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,11 +19,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Box className="w-[80%] py-[0.5rem] px-8 mt-[3rem] mb-12 mx-auto  bg-[#e4e6e9] rounded-[18px]">
-          <Link className="cursor-pointer" href="/profile">
-        <Avatar className="ml-auto" alt="Cindy Baker" src="/sampleAvatar.jpg" />
-        </Link>
       <body
         className={inter.className}
         style={{
@@ -78,32 +73,43 @@ export default function RootLayout({
                 <p>Welcome back Your name should be added</p>
               </Box>
             </Box>
-            <Avatar
-              alt="Cindy Baker"
-              src="/sampleavatar.jpg"
-              sx={{
-                height: "70px",
-                width: "70px",
-                marginLeft: "auto",
-                marginRight: "2rem",
-              }}
-            />
+            <Link className="cursor-pointer" href="/profile">
+              <Avatar
+                alt="Cindy Baker"
+                src="/sampleavatar.jpg"
+                sx={{
+                  height: "70px",
+                  width: "70px",
+                  marginLeft: "auto",
+                  marginRight: "2rem",
+                }}
+              />
+            </Link>
           </Box>
         </Box>
-        <Box className="h-[60%] w-[3rem]   border-black border-2 fixed left-[4rem] top-1/2 transform -translate-y-1/2 "></Box>
-        <section className="ml-[12rem] py-[2rem] w-[80%] flex flex-col gap-8">{children}</section>
-        </body>
         <Box
           sx={{
             background: "rgba(134,148,188,0.4)",
             borderRadius: "50px",
+            display: "flex",
           }}
-          className="h-[60%] w-[7rem]    fixed left-[4rem] top-1/2 transform -translate-y-1/2 "
+          className="h-[60%] w-[3rem]   border-black border-2 fixed left-[4rem] top-1/2 transform -translate-y-1/2 "
         ></Box>
-        <section className="ml-[12rem] w-[80%] flex flex-col gap-8">
+        <section className="ml-[12rem] py-[2rem] w-[80%] flex flex-col gap-8">
           {children}
         </section>
       </body>
+      {/* <Box
+        sx={{
+          background: "rgba(134,148,188,0.4)",
+          borderRadius: "50px",
+          display: "flex",
+        }}
+        className="h-[60%] w-[7rem]    fixed left-[4rem] top-1/2 transform -translate-y-1/2 "
+      ></Box>
+      <section className="ml-[12rem] w-[80%] flex flex-col gap-8">
+        {children}
+      </section> */}
     </html>
   );
 }
