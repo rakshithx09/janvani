fdacdb6fa9920886f98960c1986a0627c0c3cf2f
Rakshith <rakshithx09@gmail.com>
2024-03-14 23:23:19 +0530

    conflicts resolved

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Box } from "@mui/material";
+import { Avatar } from '@mui/material';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Box className="w-[80%] py-[0.5rem] px-8 mt-[3rem] mb-12 mx-auto  bg-[#e4e6e9] rounded-[18px]">
+        <Avatar className="ml-auto" alt="Cindy Baker" src="/sampleAvatar.jpg" />
+        </Box>
+        <Box className="h-[60%] w-[3rem]   border-black border-2 fixed left-[4rem] top-1/2 transform -translate-y-1/2 "></Box>
+        <section className="ml-[12rem] w-[80%] flex flex-col gap-8">{children}</section>
+        </body>
     </html>
   );
 }
