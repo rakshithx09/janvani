1f0a2893f85d61c44ecde59b27a74d23614e87c0
Rakshith <rakshithx09@gmail.com>
2024-03-15 15:02:40 +0530

    progress tracker

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,11 @@ import postRouter from "./routes/postRoutes"
 import aadhaarRouter from "./routes/aadhaarRoutes"
 import commentRouter from "./routes/commentRoutes"
 import cron from 'node-cron'
-
+import cors from 'cors'
 
 const app: Express = express()
 const port = process.env.PORT || 4000
-
+app.use(cors())
 app.use("api/v1/post/",postRouter)
 // app.use("api/v1/aadhaar/",aadhaarRouter)
 app.use("api/v1/comment/",commentRouter)
