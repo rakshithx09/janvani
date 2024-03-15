7655af5a20e2c39e73ae1b57c585ce4a5aaf2951
AnirudhKaranth <anikaranth47@gmail.com>
2024-03-16 00:59:30 +0530

    get posts by dept id

--- server/src/routes/postRoutes.ts
+++ server/src/routes/postRoutes.ts
@@ -1,11 +1,13 @@
 import express from 'express'
-import { addvote, createPost, getAllPosts, getvotes } from '../controllers/post'
+import { addvote, createPost, getAllPosts, getPostsBydept, getvotes } from '../controllers/post'
 
 const router = express.Router()
 
 router.route("/createpost").post(createPost)
 router.route("/getallposts/:userId").get(getAllPosts)
 router.route("/getvotes/:postId").get(getvotes)
+router.route("/getposts/:deptId").get(getPostsBydept)
+
 
 router.route("/addvote").post(addvote)
 
