0bc2bb3ee91ef8332d943bc07f488eafd023704d
AnirudhKaranth <anikaranth47@gmail.com>
2024-03-15 12:12:08 +0530

    post routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,20 @@ import express, { Express, Request, Response } from 'express'
 import postRouter from "./routes/postRoutes"
 import aadhaarRouter from "./routes/aadhaarRoutes"
 import commentRouter from "./routes/commentRoutes"
+import cron from 'node-cron'
 
 
 const app: Express = express()
 const port = process.env.PORT || 4000
 
 app.use("api/v1/post/",postRouter)
-app.use("api/v1/aadhaar/",aadhaarRouter)
+// app.use("api/v1/aadhaar/",aadhaarRouter)
 app.use("api/v1/comment/",commentRouter)
+
+cron.schedule('0 18 * * *', () => {
+    
+    
+});
 
 app.listen(port, () => {
     console.log(`🔥🔥🔥: Server is running at http://localhost:${port} `)
--- server/src/routes/postRoutes.ts
+++ server/src/routes/postRoutes.ts
@@ -1,9 +1,11 @@
 import express from 'express'
-import { createPost } from '../controllers/post'
+import { createPost, getAllPosts } from '../controllers/post'
 import userAuth from '../middleware/auth'
 
 const router = express.Router()
 
 router.route("/createpost").post(createPost)
+router.route("/getallposts/:userId").post(getAllPosts)
+
 
 export default router
